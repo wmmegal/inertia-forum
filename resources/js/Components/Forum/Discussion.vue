d7a25ea4e0a4a6b5cd1d8cf3d591be8ab00ab977
wmmeg <wmmegal@gmail.com>
2023-07-12 13:01:21 +0300

    chore(vite): change vite config for ssr

--- resources/js/Components/Forum/Discussion.vue
+++ resources/js/Components/Forum/Discussion.vue
@@ -1,32 +1,44 @@
 <template>
-    <div class="block bg-white p-6 text-gray-900 flex items-center space-x-6">
-        <div class="flex-grow">
-            <div class="flex items-center space-x-3">
+    <div class="block bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="p-6 text-gray-900 flex items-center space-x-6">
+            <div class="flex-grow">
+                <div class="flex items-center space-x-3">
                     <span class="inline-flex items-center rounded-lg bg-gray-100 px-3 py-0.5 text-sm text-gray-600">
                         {{ discussion.topic.name }}
                     </span>
-                <h2 class="text-lg font-medium">
-                    <template v-if="discussion.is_pinned">
-                        [Pinned]
-                    </template>
-                    {{ discussion.title }}
-                </h2>
-            </div>
-            <div class="text-gray-500 text-sm mt-3 line-clamp-1">
-                {{ discussion.post.body_preview }}
-            </div>
+                    <Link :href="`${route('discussions.show', discussion)}`" class="text-lg font-medium">
+                        <template v-if="discussion.is_pinned">
+                            [Pinned]
+                        </template>
+                        {{ discussion.title }}
+                    </Link>
+                </div>
+                <div class="text-gray-500 text-sm mt-3 line-clamp-1">
+                    {{ discussion.post.body_preview }}
+                </div>
 
-            <Link :href="`${route('discussions.show', discussion)}?post=${discussion.latest_post.id}`" class="inline-block text-sm mt-3">
-                Last post by {{ discussion.latest_post.user?.username || '[user deleted]' }} <time :datetime="discussion.latest_post.created_at.datetime" :title="discussion.latest_post.created_at.datetime">{{ discussion.latest_post.created_at.human }}</time>
-            </Link>
-        </div>
-        <div class="flex-shrink-0 flex flex-col items-end">
-            <div class="flex items-center justify-start -space-x-1">
-                <img :src="participant.avatar_url" v-for="participant in participants" :key="participant.id" class="h-6 w-6 rounded-full ring-2 ring-white first-of-type:w-7 first-of-type:h-7" :title="participant.username">
-                <span class="!ml-1 text-sm text-gray-600" v-if="discussion.participants.length > 3">+ {{ discussion.participants.length - 3 }} more</span>
+                <Link :href="`${route('discussions.show', discussion)}?post=${discussion.latest_post.id}`"
+                      class="inline-block text-sm mt-3">
+                    Last post by {{ discussion.latest_post.user?.username || '[user deleted]' }}
+                    <time :datetime="discussion.latest_post.created_at.datetime"
+                          :title="discussion.latest_post.created_at.datetime">{{
+                            discussion.latest_post.created_at.human
+                        }}
+                    </time>
+                </Link>
             </div>
-            <div class="text-sm mt-3">
-                {{ pluralize('reply', discussion.replies_count, true) }}
+            <div class="flex-shrink-0 flex flex-col items-end">
+                <div class="flex items-center justify-start -space-x-1">
+                    <img :src="participant.avatar_url" v-for="participant in participants" :key="participant.id"
+                         class="h-6 w-6 rounded-full ring-2 ring-white first-of-type:w-7 first-of-type:h-7"
+                         :title="participant.username">
+                    <span class="!ml-1 text-sm text-gray-600" v-if="discussion.participants.length > 3">+ {{
+                            discussion.participants.length - 3
+                        }} more</span>
+                </div>
+                <div class="text-sm mt-3">
+                    {{ pluralize('reply', discussion.replies_count, true) }}
+                </div>
             </div>
         </div>
     </div>
@@ -41,7 +53,7 @@ const props = defineProps({
     discussion: Object
 })
 
-const participants = computed(() => props.discussion.participants.slice(0 ,3))
+const participants = computed(() => props.discussion.participants.slice(0, 3))
 
 
 </script>
